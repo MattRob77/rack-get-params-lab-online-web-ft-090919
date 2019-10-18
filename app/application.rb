74404c824ff8fd75fd1ad255cab2ed545c9a3bfe
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-18 18:41:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif 
+    elsif req.path.match(/cart/)
       resp.write "Path Not Found"
     end
 
